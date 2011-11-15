2a5ba868178711c58f10a2fab1c08dd575c30929
root <root@tu-fe1.lanl.gov>
2011-11-15 15:46:10 -0700

    Finally figured out how to find all the nodes and switches.

--- ib2slurm.c
+++ ib2slurm.c
@@ -8,12 +8,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+
+#include <complib/cl_nodenamemap.h>
 #include <infiniband/ibnetdisc.h>
+
+void switch_iter_func(ibnd_node_t * node, void *iter_user_data)
+{
+    fprintf(stdout, "Found a switch!\n");
+}
+
+void ca_iter_func(ibnd_node_t * node, void *iter_user_data)
+{
+    fprintf(stdout, "Found a CA!\n");
+}
+
+void router_iter_func(ibnd_node_t * node, void *iter_user_data)
+{
+    fprintf(stdout, "Found a router!\n");
+}
 
 int main(int argc, char** argv)
 {
     struct ibnd_config config = {0};
     ibnd_fabric_t *fabric = NULL;
+
+    nn_map_t *node_name_map = NULL;
+    char *node_name_map_file = NULL;
 
     char *ibd_ca = NULL;
     int ibd_ca_port = 0;
@@ -28,33 +48,21 @@ int main(int argc, char** argv)
     while((c = getopt_long(argc, argv, "m:", long_options, &option_index)) != -1) {
         switch(c) {
             case 'm':
-                printf ("option m with value '%s'\n", optarg);
+                node_name_map_file = strdup(optarg);
                 break;
         }
     }
+
+    node_name_map = open_node_name_map(node_name_map_file);
 
     if ((fabric = ibnd_discover_fabric(ibd_ca, ibd_ca_port, NULL, &config)) == NULL) {
         fprintf(stderr, "IB discover failed.\n");
         exit(EXIT_FAILURE);
     }
 
-    fprintf(stdout, "Max hops = %u\n", fabric->maxhops_discovered);
-
-    ibnd_chassis_t *ch = NULL;
-    ibnd_node_t *node;
-    for (ch = fabric->chassis; ch; ch = ch->next) {
-        fprintf(stdout, "Found a chassis!\n");
-
-        int i = 0;
-        ibnd_node_t *node;
-
-        for (node = ch->nodes; node; node = node->next_chassis_node) {
-            fprintf(stdout, "Found a node!\n");
-            if (node->type == IB_NODE_SWITCH) {
-                fprintf(stdout, "Found a switch!\n");
-            }
-        }
-    }
+    ibnd_iter_nodes_type(fabric, switch_iter_func, IB_NODE_SWITCH, NULL);
+    ibnd_iter_nodes_type(fabric, ca_iter_func, IB_NODE_CA, NULL);
+    ibnd_iter_nodes_type(fabric, router_iter_func, IB_NODE_ROUTER, NULL);
 
     ibnd_destroy_fabric(fabric);
     exit(EXIT_SUCCESS);
